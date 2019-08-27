6d3dc191f29058e2ebde36829e9b4cf3112435a7
Matteo Ruina <matteo.ruina@gmail.com>
2019-08-27 21:30:24 +0200

    Output which jsonnet file changes

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # pre-commit
-Collection of pre-commit hooks
+
+Collection of pre-commit hooks for [https://github.com/pre-commit/pre-commit](https://github.com/pre-commit/pre-commit).
+
+Hooks:
+
+- **jsonnet-fmt**: format a `*.jsonnet` file using `jsonnetfmt`
--- hooks/jsonnet-fmt.sh
+++ hooks/jsonnet-fmt.sh
@@ -3,5 +3,15 @@
 set -e
 
 for file in "$@"; do
+
+    # We are using the md5sum to check if the file is changing before and after the formatting
+    # because jsonnetfmt doesn't have a different exit code to differentiate.  
+
+    before=$(md5 -q "${file}")
     jsonnetfmt -i "${file}"
+    after=$(md5 -q "${file}")
+
+    if [[ ${after} != "${before}" ]]; then
+        echo "Fixing ${file}"
+    fi
 done
